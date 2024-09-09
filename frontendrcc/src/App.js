e6d304ef75ef700a2a9eb9ab0ffeb4c4e56029be
shibanis <shibani1994@gmail.com>
2024-09-09 03:38:22 -0700

    front end

---
+++ frontendrcc/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [articleInfo, setArticleInfo] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`http://localhost:8000/api/article/article1`);
+      setArticleInfo(response.data);
+      console.log("SHibani")
+    }
+    fetchData();
+
+  }, [])
+  return (
+    <div className="App">
+      Reliance Cardamom Company
+      {
+        articleInfo && articleInfo.name ? <>{articleInfo.name}</> : <>No Articlefound</>
+      }
+    </div>
+  );
+}
+
+export default App;
